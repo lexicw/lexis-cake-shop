406401eab82de9ade89000276cfd73083dba6d7d
lexicw <42586346+lexicw@users.noreply.github.com>
2023-09-09 14:03:02 -0400

    Added subtle gradient to review cards

--- components/ReviewCard.vue
+++ components/ReviewCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="review-card mb-3 w-full lg:mx-auto sm:mb-0 border border-gray-200 text-center relative shadow-lg bg-white">
+    <div class="review-card mb-3 w-full lg:mx-auto sm:mb-0 border border-gray-200 text-center relative shadow-lg">
       <div class="review-stars text-yellow-500 flex justify-center mt-4 lg:mt-12">
           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-star-fill inline-flex m-1" viewBox="0 0 16 16">
             <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
@@ -40,6 +40,8 @@ const { title, content } = defineProps({
   aspect-ratio: 1 / 1;
   overflow: hidden;
   position: relative;
+  background: rgb(255,215,232);
+background: linear-gradient(0deg, rgba(255,215,232,1) 0%, rgba(255,255,255,1) 40%);
 }
 
 .review-card:hover .review-card-content {
